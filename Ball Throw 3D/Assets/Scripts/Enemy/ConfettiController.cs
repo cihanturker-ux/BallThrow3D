a65362304d63b16bfea330880b0fa38b4c051503
Enes Telli <enes1903telli@gmail.com>
2021-04-28 17:41:50 +0300

    Project folder added

---
+++ Ball Throw 3D/Assets/Scripts/Enemy/ConfettiController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfettiController : MonoBehaviour
+{
+    public static ConfettiController instance;
+
+    [SerializeField] private GameObject confettis;
+
+    private List<Transform> confettiList;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        confettiList = new List<Transform>();
+
+        foreach (var child in confettis.transform)
+        {
+            confettiList.Add((Transform)child);
+        }
+    }
+
+    public void ActivateConfetti()
+    {
+        confettiList[0].gameObject.active = true;
+        confettiList.Remove(confettiList[0]);
+    }
+}
